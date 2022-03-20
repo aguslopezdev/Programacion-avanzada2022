31d1ecd96eb6e4123e2d9f9c8994ec26c275acc0
Agustin Lopez <agustinlopezprieto@gmail.com>
2022-03-20 18:08:36 -0300

    Comentarios añadidos al codigo

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,3 +1,8 @@
+/**
+ * Muestra por pantalla la frase "Hola Mundo"
+ * 
+ * @author Agustin Lopez
+ */
 public class HolaMundo {
     public static void main (String[] args) {
         System.out.println("Hola mundo");
