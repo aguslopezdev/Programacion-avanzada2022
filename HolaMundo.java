04c5c18d730b748a850b857720b1f142aa8f38d4
Agustin Lopez <agustinlopezprieto@gmail.com>
2022-03-20 19:27:53 -0300

    Saludo ampliado

--- HolaMundo.java
+++ HolaMundo.java
@@ -6,5 +6,6 @@
 public class HolaMundo {
     public static void main (String[] args) {
         System.out.println("Hola mundo");
+        System.out.println("Aqui estamos haciendo pruebas con Git.");
     }
 }
