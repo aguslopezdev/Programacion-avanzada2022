815f183762bbed0a7aa22203a8446a09e3c5d9c0
Agustin Lopez <agustinlopezprieto@gmail.com>
2022-03-20 20:00:40 -0300

    Colores definidos

--- HolaMundo.java
+++ HolaMundo.java
@@ -5,7 +5,9 @@
  */
 public class HolaMundo {
     public static void main (String[] args) {
-        System.out.println("Hola mundo");
-        System.out.println("Aqui estamos haciendo pruebas con Git.");
+        String naranja = "\033[33m";
+        String azul = "\033[34m";
+        System.out.println(naranja + "Hola mundo");
+        System.out.println(azul + "Aqui estamos haciendo pruebas con Git.");
     }
 }
